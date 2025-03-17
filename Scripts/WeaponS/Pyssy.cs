8bd32b5038eb56430549c2431d138a117a15be33
Panu Ohra-aho <panu.ohraaho@gmail.com>
2025-03-17 14:38:03 +0200

    Some more weapons added

---
+++ Scripts/WeaponS/Pyssy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyssy : MonoBehaviour
+{
+    public void IncreaseStack()
+    {
+        if(GetComponent<Weapon>().stacks <= 6)
+        {
+            GetComponent<Weapon>().stacks++;
+            GetComponent<Weapon>().damage = 6 - GetComponent<Weapon>().stacks;
+            if (GetComponent<Weapon>().damage < 0)
+            {
+                GetComponent<Weapon>().damage = 0;
+            }
+        }
+    }
+}
