2eee5ad034f2d6723343aa73ab9d40706fe2ab31
Panu Ohra-aho <panu.ohraaho@gmail.com>
2025-10-15 17:50:58 +0300

    Huge art update

--- Scripts/WeaponS/Proteesi.cs
+++ Scripts/WeaponS/Proteesi.cs
@@ -6,7 +6,7 @@ public class Proteesi : MonoBehaviour
 {
     public void HealthIncrease()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerContoller>().HB.IncreaseHealthBar(3, true);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerContoller>().HB.IncreaseHealthBar(2, true);
         GetComponent<SelfDestruct>().Destruct();
     }
 }
