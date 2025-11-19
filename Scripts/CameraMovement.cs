c52779bc61db0aaca6c8d900932ff645ac7339e5
Panu Ohra-aho <panu.ohraaho@gmail.com>
2025-11-19 18:58:44 +0200

    camera pan fixed

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     float pan_limit = 0.3f;
-    float speed = 0.02f;
+    float speed = 0.003f;
     float move_trigger = 3f;
 
     public bool disabled = false;
@@ -29,20 +29,20 @@ public class CameraMovement : MonoBehaviour
                 float distance = Mathf.Sqrt((m_x * m_x + m_y * m_y));
                 if (m_x > move_trigger && transform.position.x < pan_limit)
                 {
-                    Vector3 pos = new Vector3(transform.position.x + m_x / distance/3 * (speed / 2), transform.position.y, transform.position.z);
+                    Vector3 pos = new Vector3(transform.position.x + m_x / distance/3 * (speed * Time.deltaTime * Screen.width), transform.position.y, transform.position.z);
                     transform.position = pos;
                     background.transform.position = new Vector3(pos.x*0.3f, background.transform.position.y, 0);
                 }
                 else if (m_x < -move_trigger && transform.position.x > -pan_limit)
                 {
-                    Vector3 pos = new Vector3(transform.position.x + m_x / distance/3 * (speed / 2), transform.position.y, transform.position.z);
+                    Vector3 pos = new Vector3(transform.position.x + m_x / distance/3 * (speed*Time.deltaTime*Screen.width), transform.position.y, transform.position.z);
                     transform.position = pos;
                     background.transform.position = new Vector3(pos.x*0.3f, background.transform.position.y, 0);
                 }
             }
             else if (m_x < move_trigger && m_x > -move_trigger && transform.position != new Vector3(0, 0, -1))
             {
-                Vector3 pos = new Vector3(transform.position.x - transform.position.x * speed, transform.position.y - transform.position.y * speed, transform.position.z);
+                Vector3 pos = new Vector3(transform.position.x - transform.position.x * (speed * Time.deltaTime * Screen.width), transform.position.y - transform.position.y * speed, transform.position.z);
                 transform.position = pos;
                 background.transform.position = new Vector3(pos.x*0.3f, background.transform.position.y, 0);
             }
