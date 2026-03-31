9ecc58edfbb53142e26e402de6c5270c4652b564
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-03-31 20:57:15 +0300

    catapult updated

--- Scripts/WeaponS/Catapult.cs
+++ Scripts/WeaponS/Catapult.cs
@@ -6,12 +6,13 @@ public class Catapult : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<BuffController>().buff_requirement = (Weapon w) => { return w.type == MainController.Choise.kivi; };
+        GetComponent<BuffController>().buff_requirement = (Weapon w) => { return w.name != GetComponent<Weapon>().name; };
         GetComponent<BuffController>().timer = 2;
         GetComponent<BuffController>().temporary = true;
         GetComponent<BuffController>().special_apply = true;
         GetComponent<BuffController>().special = DealDamage;
         GetComponent<BuffController>().endPhase = true;
+        GetComponent<BuffController>().destructive = true;
     }
 
     public void AppluBuffs()
