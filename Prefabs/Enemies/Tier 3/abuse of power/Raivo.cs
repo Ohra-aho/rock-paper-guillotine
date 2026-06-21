f21fd7bfa444798948508c2e06618ff86547165b
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-06-21 23:15:25 +0300

    one more enemys weapons made

--- Prefabs/Enemies/Tier 3/abuse of power/Raivo.cs
+++ Prefabs/Enemies/Tier 3/abuse of power/Raivo.cs
@@ -4,40 +4,32 @@ using UnityEngine;
 
 public class Raivo : MonoBehaviour
 {
-    private void Awake()
-    {
-        //GetComponent<BuffController>().special_removal = Remove;
-        GetComponent<BuffController>().special = Empower;
-        GetComponent<BuffController>().takeDamage = true;
-        GetComponent<BuffController>().buff_requirement = (Weapon weapon) => { return true; };
-    }
+	void Awake()
+	{
+		if(GetComponent<BuffController>())
+		{
+			GetComponent<BuffController>().buff_requirement = (Weapon w) => { return true; };
+			GetComponent<BuffController>().type_change = MainController.Choise.voittamaton;
+			GetComponent<BuffController>().temporary = true;
+			GetComponent<BuffController>().timer = 3;
+			GetComponent<BuffController>().special_apply = true;
+		}
+	}
+	public void Corrupt()
+	{
+		GetComponent<Weapon>().owner.HB.InstaKill();
+	}
 
-    public void Empower(Weapon weapon)
-    {
-       
-        GameObject true_weapon_holder = GameObject.FindGameObjectWithTag("RIE");
-        for (int i = 0; i < true_weapon_holder.transform.childCount; i++)
-        {
-            Buff new_buff = Instantiate(GetComponent<BuffController>().buff, true_weapon_holder.transform.GetChild(i)).GetComponent<Buff>();
-            new_buff.temporary = true;
-            new_buff.timer = 2;
-            new_buff.damage_buff = 2;
-            new_buff.id = GetComponent<Weapon>().name + "_2";
-            new_buff.AddBuff();
-        }
-    }
+	public void Radiance()
+	{
+		GetComponent<Stacking>().IncreaseStacks(1);
+		GetComponent<EffectDamage>().amount = GetComponent<Stacking>().stacks;
+		GetComponent<EffectDamage>().DealDamage(GetComponent<Weapon>());
+		GetComponent<EffectDamage>().SelfDamage(GetComponent<Weapon>());
+	}
 
-    public void Debuff()
-    {
-        GameObject true_weapon_holder = GameObject.FindGameObjectWithTag("RIE");
-        for (int i = 0; i < true_weapon_holder.transform.childCount; i++)
-        {
-            Buff new_buff = Instantiate(GetComponent<BuffController>().buff, true_weapon_holder.transform.GetChild(i)).GetComponent<Buff>();
-            new_buff.temporary = true;
-            new_buff.timer = 2;
-            new_buff.damage_buff = -1;
-            new_buff.id = GetComponent<Weapon>().name + "_3";
-            new_buff.AddBuff();
-        }
-    }
+	public void Untouch()
+	{
+		
+	}
 }
--- Prefabs/Enemies/Tier 3/riot/MustaRitari.cs
+++ Prefabs/Enemies/Tier 3/riot/MustaRitari.cs
@@ -1,44 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MustaRitari : MonoBehaviour
 {
-    GameObject controller;
-    GameObject RIE;
+	public GameObject buff;
+	int weaknesses = 0;
+	int poisons = 0;
+	int bleeds = 0;
+    public void Beacon()
+	{
+		GetComponent<Weapon>().opponent.type = MainController.Choise.voittamaton;
+		GetComponent<Weapon>().opponent.AddComponent<SelfDestruct>();
+		GetComponent<Weapon>().opponent.endPhase.AddListener(GetComponent<Weapon>().opponent.GetComponent<SelfDestruct>().Destruct);
+	}
 
-    public bool buffed;
+	public void Trample()
+	{
+		weaknesses = 0;
+		poisons = 0;
+		bleeds = 0;
+		GameObject RI = GameObject.FindGameObjectWithTag("RI");
+		for(int i = 0; i < RI.transform.childCount; i++)
+		{
+			if(RI.transform.GetChild(i).GetComponent<Weapon>().name == "Weakness")
+			{
+				weaknesses++;
+			}
+			if(RI.transform.GetChild(i).GetComponent<Weapon>().name == "Poison")
+			{
+				poisons++;
+			}
+			if(RI.transform.GetChild(i).GetComponent<Weapon>().name == "Bleed")
+			{
+				bleeds++;
+			}
+		}
 
-    private void Awake()
-    {
-        controller = GameObject.FindGameObjectWithTag("EnemyHolder");
-        controller.GetComponent<EnemyController>().choiseMaker = MakeChoise;
-        RIE = GameObject.FindGameObjectWithTag("RIE");
-    }
+		GameObject w_buff = GetComponent<Weapon>().GetCertainBuff("w_buff");
+		GameObject p_buff = GetComponent<Weapon>().GetCertainBuff("p_buff");
+		GameObject b_buff = GetComponent<Weapon>().GetCertainBuff("b_buff");
 
-    private void Update()
-    {
-        bool found = false;
-        for(int i = 0; i < RIE.transform.childCount; i++)
-        {
-            if(RIE.transform.GetChild(i).childCount > 0)
-            {
-                found = true;
-                break;
-            }
-        }
-        buffed = found;
-    }
+		w_buff.GetComponent<Buff>().RemoveBuff();
+		p_buff.GetComponent<Buff>().RemoveBuff();
+		b_buff.GetComponent<Buff>().RemoveBuff();
 
-    public int MakeChoise(MainController.Choise playerChoise)
-    {
-        if (buffed)
-        {
-            return 0;
-        }
-        else
-        {
-            return GetComponent<BasicEnemy>().MakeChoise(playerChoise);
-        }
-    }
+		w_buff.GetComponent<Buff>().damage_buff = weaknesses*2;
+		p_buff.GetComponent<Buff>().armor_buff = poisons;
+		if(bleeds > 0) b_buff.GetComponent<Buff>().draw_winner = true;
+		else b_buff.GetComponent<Buff>().draw_winner = false;
+
+		w_buff.GetComponent<Buff>().AddBuff();
+		p_buff.GetComponent<Buff>().AddBuff();
+		b_buff.GetComponent<Buff>().AddBuff();
+	}
+
+	public void ApplyTrampleBuffs()
+	{
+		Buff w_buff = Instantiate(buff, transform).GetComponent<Buff>();
+		w_buff.id = "w_buff";
+		w_buff.damage_buff = 0;
+		w_buff.AddBuff();
+
+		Buff p_buff = Instantiate(buff, transform).GetComponent<Buff>();
+		p_buff.id = "p_buff";
+		p_buff.armor_buff = 0;
+		p_buff.AddBuff();
+
+		Buff b_buff = Instantiate(buff, transform).GetComponent<Buff>();
+		b_buff.id = "b_buff";
+		b_buff.draw_winner = false;
+		b_buff.AddBuff();
+	}
 }
