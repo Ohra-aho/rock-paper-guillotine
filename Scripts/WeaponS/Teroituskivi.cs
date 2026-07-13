adecb20f574d8d69725b02932e93878819f3ba58
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-07-13 16:12:20 +0300

    whetstone updated

--- Scripts/WeaponS/Teroituskivi.cs
+++ Scripts/WeaponS/Teroituskivi.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Teroituskivi : MonoBehaviour
 {
-    public int damage_bonus;
 
     private void Awake()
     {
-        GetComponent<BuffController>().damage_bonus = damage_bonus;
+        GetComponent<BuffController>().effect_damage_bonus = 1;
         GetComponent<BuffController>().buff_requirement = (Weapon weapon) => { return weapon.type == MainController.Choise.sakset; };
+		GetComponent<BuffController>().temporary = true;
+		GetComponent<BuffController>().timer = 3;
+		GetComponent<BuffController>().special_apply = true;
+		GetComponent<BuffController>().reminder = "+" + GetComponent<BuffController>().effect_damage_bonus + " effect damage.";
     }
 
 
