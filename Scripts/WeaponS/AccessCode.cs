fbdcf5d02c4b32f04992f7f74e7468f4a3d98e80
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-09-05 17:40:43 +0300

    access code updated

--- Scripts/WeaponS/AccessCode.cs
+++ Scripts/WeaponS/AccessCode.cs
@@ -6,11 +6,21 @@ public class AccessCode : MonoBehaviour
 {
 	void Awake()
 	{
-		GetComponent<BuffController>().penetrating = true;
-		GetComponent<BuffController>().timer = 3;
+		GetComponent<BuffController>().timer = 2;
 		GetComponent<BuffController>().temporary = true;
 		GetComponent<BuffController>().buff_requirement = (Weapon w) => { return true; };
 		GetComponent<BuffController>().special_apply = true;
-		GetComponent<BuffController>().reminder = "Pierces armor.";
+		GetComponent<BuffController>().type_change = MainController.Choise.voittamaton;
+		GetComponent<BuffController>().endPhase = true;
+		GetComponent<BuffController>().special = (Weapon w) =>
+		{
+			Buff old_buff = w.GetCertainBuff(GetComponent<Weapon>().name).GetComponent<Buff>();
+			old_buff.RemoveBuff();
+			old_buff.type_change = MainController.Choise.useless;
+			old_buff.timer = 1000;
+			old_buff.reminder = "Made useless until the end of the fight.";
+			old_buff.AddBuff();
+		};
+		GetComponent<BuffController>().reminder = "After use, become \"useless\" for the rest of the fight.";
  	}
 }
