68a4162f46f5be7e28e5f6470087ceafcd62fd67
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-09-05 16:01:05 +0300

    flag updated

--- Scripts/WeaponS/Lippu.cs
+++ Scripts/WeaponS/Lippu.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class Lippu : MonoBehaviour
 {
-    bool debuffed = false;
+    private void Awake()
+    {
+        GetComponent<BuffController>().special = Stacking;
+        GetComponent<BuffController>().endPhase = true;
+        GetComponent<BuffController>().buff_requirement = (Weapon w) => { return true; };
+    }
 
-	void Awake()
-	{
-		GetComponent<BuffController>().damage_bonus = 1;
-		GetComponent<BuffController>().temporary = true;
-		GetComponent<BuffController>().timer = 1000;
-		GetComponent<BuffController>().lose = true;
-		GetComponent<BuffController>().special = (Weapon w) => { GetComponent<BuffController>().Unequip(); };
-		GetComponent<BuffController>().reminder = "+1 damage until you lose with any weapon.";
-		GetComponent<BuffController>().buff_requirement = (Weapon w) => { return true; };
-		GetComponent<BuffController>().special_apply = true;
-	}
+    public void Stacking(Weapon w)
+    {
+        MainController MC = GameObject.FindGameObjectWithTag("GameController").GetComponent<MainController>();
+        if(MC.won == true)
+        {
+            AppluBuffs();
+        }
+
+    }
+
+    public void AppluBuffs()
+    {
+        GameObject RI = GameObject.FindGameObjectWithTag("RI");
+        for(int i = 0; i < RI.transform.childCount; i++)
+        {
+            Buff new_buff = Instantiate(GetComponent<BuffController>().buff, RI.transform.GetChild(i)).GetComponent<Buff>();
+            new_buff.id = GetComponent<Weapon>().name+"_2";
+            new_buff.temporary = true;
+            new_buff.timer = 1000;
+            new_buff.special = (Weapon w) => { RemoveDamageBuffs(); };
+            new_buff.lose = true;
+            new_buff.armor_buff = 1;
+            new_buff.AddBuff();
+        }
+    }
+
+    public void RemoveDamageBuffs()
+    {
+        GameObject RI = GameObject.FindGameObjectWithTag("RI");
+        for(int i = 0; i < RI.transform.childCount; i++)
+        {
+            for(int j = RI.transform.GetChild(i).transform.childCount-1; j >= 0; j--)
+            {
+                if(RI.transform.GetChild(i).transform.GetChild(j).GetComponent<Buff>().id == GetComponent<Weapon>().name + "_2")
+                {
+                    RI.transform.GetChild(i).transform.GetChild(j).GetComponent<Buff>().RemoveBuff(); 
+                    Destroy(RI.transform.GetChild(i).transform.GetChild(j).gameObject);
+                }
+            }
+        }
+    }
 }
